5c57ed411918bfc5c2c12123d2d58fe97c38bd47
Bruno Cardozo <bruno.cardozo.pereira@gmail.com>
2022-08-13 13:04:17 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/brunocp/course/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.brunocp.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.brunocp.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	
+}
